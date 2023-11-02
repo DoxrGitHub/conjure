d6cba035068b3ecde6508e6fe1a74240f90433d7
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-11-02 18:52:11 +0000

    analytics

--- online/user.js
+++ online/user.js
@@ -1,3 +1,4 @@
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 console.log(window.location.href)
@@ -15,3 +16,9 @@ if (urlParams.get('pass') === 'daddydoxr') {
   }
   window.location.replace(document.location.origin);
 }
+
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "jk96hs8oij");
