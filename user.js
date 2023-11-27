5b23a957253e636fa1769f456332d3ebb9f26094
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-11-27 13:22:22 -0600

    ew, non chromebook users blocked my site

--- user.js
+++ user.js
@@ -3,7 +3,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 console.log(window.location.href)
 
-/* if (urlParams.get('pass') !== 'daddydoxr') {
+if (urlParams.get('pass') !== 'daddydoxr') {
   if (localStorage.getItem('foreignAuth') === null){
   if (/\bCrOS\b/.test(navigator.userAgent)) {
 } else {
@@ -15,7 +15,7 @@ if (urlParams.get('pass') === 'daddydoxr') {
     localStorage.setItem("foreignAuth", "true");
   }
   window.location.replace(document.location.origin);
-} */
+}
 
 (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
